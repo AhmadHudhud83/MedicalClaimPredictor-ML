d6aca231c409521a7e27c0cb479d5dbc948b516f
AhmadHudhud83 <151885597+AhmadHudhud83@users.noreply.github.com>
2024-12-02 18:39:35 +0200

    initialized first two stages of the project pipeline

---
+++ src/feature_engineering.py
@@ -0,0 +1,14 @@
+# Importing Dependencies
+import pandas as pd
+import numpy as np
+
+
+#Reading the Processed Train data
+processed_train_data = pd.read_csv('./data/processed/train_processed.csv')
+#Reading the Processed Test data 
+processed_test_data = pd.read_csv('./data/processed/test_processed.csv')
+
+
+
+
+
